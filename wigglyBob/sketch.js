8ccded2e4dfb5bcd269f121615532b3388faaa69
Lillian Ritchie <42877489+lillianritchie@users.noreply.github.com>
2019-02-17 11:41:21 -0500

    got one petal rotating and oscillating

--- wigglyBob/sketch.js
+++ wigglyBob/sketch.js
@@ -1,6 +1,6 @@
 // modifiable global variables
 let myNickname = "wigglyBob";
-let nFramesInLoop = 120;
+let nFramesInLoop = 200;
 let bEnableExport = true;
 //don't touch these globals
 let nElapsedFrames;
@@ -55,28 +55,37 @@ function renderMyDesign(percent) {
 	stroke(0, 0, 0);
 	strokeWeight(2);
 	//
-	let cx = 100;
-	let cy = 100;
+	let cx = 200;
+	let cy = 200;
 	//rotating element - look at this to figure out petals
-	let radius = 80;
-	let rotatingArmAngle = percent * TWO_PI;
-	let px = cx + radius * cos(rotatingArmAngle);
-	let py = cy - radius * sin(rotatingArmAngle);
-	fill(255);
-	line(cx, cy, px, py);
-	ellipse(px, py, 20, 20);
+	// rotation variables
 
+	//colorMode(HSB);
+	push();
+	ellipseMode(CENTER);
+	let rotatingAngle = percent * TWO_PI;
+	let px = cx + 30 * cos(rotatingAngle);
+	let petalRadius = map(sin(rotatingAngle),-1,1,125,150);
+	translate(width / 2, height / 2);
+	rotate(rotatingAngle)
+
+	fill(255);
+	ellipse(petalRadius-50,0, petalRadius, 50);
+	
+	pop();
+	fill(255,255,0);
+	ellipse(width/2, height/2, 100,100);
 	//pulsating ellipse - maybe add some leaves? or facial expression
-	let ellipsePulse = sin(3.0 * percent * TWO_PI);
-	let ellipseW = map(ellipsePulse, -1, 1, 20, 50);
-	let ellipseH = map(ellipsePulse, -1, 1, 50,30);
-	let ellipseColor = map(ellipsePulse, -1,1, 128,255);
-	fill(255,ellipseColor,ellipseColor);
-	ellipse(cx,cy,ellipseW,ellipseH);
+	// let ellipsePulse = sin(3.0 * percent * TWO_PI);
+	// let ellipseW = map(ellipsePulse, -1, 1, 20, 50);
+	// let ellipseH = map(ellipsePulse, -1, 1, 50,30);
+	// let ellipseColor = map(ellipsePulse, -1,1, 128,255);
+	// fill(255,ellipseColor,ellipseColor);
+	// ellipse(cx,cy,ellipseW,ellipseH);
 
 	//visual feedback
-	fill(255,0,0);
+	fill(255, 0, 0);
 	noStroke();
-	let percentDisplayString = "" + nf(percent,1,3);
-	text (percentDisplayString, 5, 15);
+	let percentDisplayString = "" + nf(percent, 1, 3);
+	text(percentDisplayString, 5, 15);
 }
