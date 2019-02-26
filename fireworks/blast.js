e445a118b6ec43e7e143cffd74175e69f7356115
Lillian Ritchie <42877489+lillianritchie@users.noreply.github.com>
2019-02-25 19:13:39 -0500

    Add files via upload

---
+++ fireworks/blast.js
@@ -0,0 +1,22 @@
+class Blast extends Particle {
+  constructor(x,y,hu) {
+    super();
+    this.vel = p5.Vector.random2D();
+    this.vel.mult(random(2,10));
+  }
+
+  update(){
+    this.vel.mult(0.9);
+    this.lifespan -= 4;
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+
+  show() {
+    strokeWeight(2);
+    stroke(this.hu, 100,100, this.lifespan);
+
+    point(this.pos.x, this.pos.y);
+  }
+}
