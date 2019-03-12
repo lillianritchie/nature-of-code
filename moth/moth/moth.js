c86fa9080ac7b52837a35cf02500805ded316f96
Lillian Ritchie <42877489+lillianritchie@users.noreply.github.com>
2019-03-11 23:43:18 -0400

    Add files via upload

---
+++ moth/moth/moth.js
@@ -0,0 +1,106 @@
+class Moth {
+  constructor() {
+    this.position = createVector(random(width), random(0, height));
+    this.noff = createVector(random(100), 1);
+    this.speed = random(0, 0.02);
+    this.particles = [];
+    this.done = false;
+  }
+
+  update() {
+    this.position.x = map(noise(this.noff.x), 0, 1, -200, width + 200);
+    this.position.y = this.position.y + random(-5, 5);
+    this.noff.add(this.speed, this.speed);
+
+    if (this.position.y < 0 && this.position.x < width * 3 / 8 && this.position.x > width * 5 / 8) {
+      this.position.y = this.position.y + height;
+    }
+  }
+
+  display() {
+    fill(150, 150, 150, 100);
+    ellipse(this.position.x, this.position.y, 17);
+    ellipse(this.position.x, this.position.y, 25);
+    ellipse(this.position.x, this.position.y, 30);
+  }
+
+  dead() {
+    if (this.position.x >= 3 / 8 * width && this.position.x <= 5 / 8 * width && this.position.y <= height / 20) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  zap() {
+    if (this.dead()) {
+      for (let i = 0; i < 50; i++) {
+        const d = new Dust(this.position.x, this.position.y);
+        this.particles.push(d); // particles is an array of Dust.
+      }
+    }
+
+  }
+
+  displayDust() {
+    //for loop of all particles
+    //call update
+    //display
+    this.particles.forEach(p => {
+      p.update()
+      p.display()
+
+      if (p.position.y >= height){
+        this.done = true;
+      }
+    })
+  }
+
+}
+
+class Dust {
+  constructor(x, y) {
+    this.position = createVector(x, y);
+    this.velocity = createVector(random(-0.4,0.4), random(0.01,0.4));
+    this.velocity.mult(random(1, 5));
+    this.acceleration = createVector(0, 0.1);
+    this.alpha = map(0,height, 255,0);
+  }
+
+  update() {
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    // this.acceleration.mult(0);
+  }
+
+  display() {
+    if (this.position.x >= 3 / 8 * width && this.position.x <= 5 / 8 * width && this.position.y <= height / 20){
+      fill(255,255,150,this.alpha);
+      ellipse(this.position.x, this.position.y,15);
+    } else {
+    fill(100, 100, 100, 100);
+    ellipse(this.position.x, this.position.y,7);
+    }
+  }
+}
+
+class Survivor {
+  constructor() {
+    this.position = createVector(random(width), random(0, height));
+    this.noff = createVector(random(100), random(10));
+    this.speed = 0.01;
+  }
+
+  update() {
+    this.position.x = map(noise(this.noff.x), 0, 1, -200, width + 200);
+    this.position.y = map(noise(this.noff.y), 0, 1, height, 0)
+    this.noff.add(this.speed, this.speed);
+  }
+
+  display() {
+    fill(150, 150, 150, 100);
+    ellipse(this.position.x, this.position.y, 5);
+    ellipse(this.position.x, this.position.y, 7);
+    ellipse(this.position.x, this.position.y, 10);
+  }
+}
---
+++ moth/moth/sketch.js
@@ -0,0 +1,51 @@
+let moths = [];
+let survivors = [];
+//let walker1;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  for (i = 0; i < 20; i++) {
+    moths.push(new Moth());
+    survivors.push(new Survivor());
+  }
+  noStroke();
+
+
+}
+
+function draw() {
+  background(0, 0, 0, 100)
+  fill(255, 230, 0, 205);
+  //ellipse(width / 2, 0, width / 3, height / 8)
+  ellipse(width / 2, 0, width / 4, height / 10);
+  fill(255, 230, 0, 50)
+  triangle(width / 2, -height / 2, width / 8, height, width * 7 / 8, height);
+
+  for (j=0; j < survivors.length; j++) {
+    survivors[j].update();
+    survivors[j].display();
+  }
+
+  
+  for (i = 0; i < moths.length; i++) {
+  
+    if (moths[i].dead()) {
+      // dead flow
+      moths[i].displayDust();
+    } else {
+      // alive flow
+      moths[i].update();
+      moths[i].display();
+      moths[i].zap();
+    }
+
+    if (moths[i].done){
+      moths.splice(i,1);
+      moths.push(new Moth());
+    }
+  
+  }
+  
+    
+  }
